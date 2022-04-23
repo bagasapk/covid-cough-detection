a98a84b9df37861d4f6a41eb025c30bf368a7c2a
bagasapk <bagasapk>
2022-04-23 15:58:13 +0700

    add responsive contacts

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('coughvid.html')
+    return render_template('contact.html')
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
