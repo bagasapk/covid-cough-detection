33bec59f3aab4d51073519aa8feb896b3ad3bf9e
bagasapk <bagasapk>
2022-04-29 07:55:18 +0700

    90% Connect to BE

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from datetime import datetime
 import re
-from flask import render_template
+import os
+import uuid
+import io
 
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from flask import render_template,flash,request,redirect,Response,session
+from convert_audio_2_spec import convert_audio_to_mel
+from convert_audio_2_spec import preprocess
+from convert_audio_2_spec import generategraph
+from convert_audio_2_spec import get_melspec
+from convert_audio_2_spec import printPredict
+
+UPLOAD_FOLDER = 'static/files'
 
 app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
-    return render_template('contact.html')
+    return render_template('test.html')
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
@@ -22,3 +36,25 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+@app.route('/',methods = ['POST'])
+def save_record():
+    if 'file' not in request.files:
+        flash('No File Part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No Selected File')
+        return redirect(request.url)
+    file_name = str(uuid.uuid4()) + ".wav"
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    feature = convert_audio_to_mel(full_file_name)
+    feature = preprocess(feature,featureSize = (224,224))
+    fig = generategraph(file_name,feature)
+    feature = get_melspec(file_name)
+    feature = feature.reshape(-1,224,224,3)
+    session['name'] = []
+    session['name'].append(file_name)
+    # printPredict(feature)
+    return "Success"
---
+++ convert_audio_2_spec.py
@@ -0,0 +1,83 @@
+import librosa
+import cv2
+from sklearn.preprocessing import StandardScaler
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask
+import os
+
+UPLOAD_PLT = "static/plt"
+
+app = Flask(__name__)
+app.config['UPLOAD_PLT'] = UPLOAD_PLT
+
+def white_noise(y):
+    wn = np.random.randn(len(y))
+    y_wn = y + random.uniform(0, 0.005)*wn
+    return y_wn
+
+def time_shift(y):
+    y = np.roll(y, random.randint(-10000,10000))
+    return y
+
+def Gain(y):
+    y = y + random.uniform(-0.2,0.2)*y
+    return y       
+                            
+def stretch(y, rate=random.uniform(0.8,1.2)):
+    y = librosa.effects.time_stretch(y, rate)
+    return y
+
+def convert_audio_to_mel(filename):
+    y,sr = librosa.load(filename, mono=True, duration=10)
+    for i in range(1):
+        chance = random.randint(0,100)
+        if chance <=20:
+            stretch(y)
+        if chance <=40:
+            time_shift(y)
+        if chance <=60:
+            Gain(y)
+        if chance <=80:
+            white_noise(y)
+        melspec = librosa.feature.melspectrogram(y, sr=sr,n_fft=2048, hop_length=512,n_mels=128)
+        feature = librosa.power_to_db(melspec, ref=np.max)
+    return feature
+
+def get_melspec(filename):
+    full_file_name = os.path.join(app.config['UPLOAD_PLT'], filename)
+    im = plt.imread(full_file_name+'.png')
+    im = cv2.cvtColor(im, cv2.COLOR_BGRA2BGR)
+    return im
+
+
+def preprocess(feature, featureSize):
+    scaler = StandardScaler()
+    widthTarget, heightTarget = featureSize #224,224
+    height, width = feature.shape[0], feature.shape[1] #1193,431
+  
+    newSize = (224, 224) #107,41
+    feature = cv2.resize(feature, newSize) 
+    # Normalization
+    feature = scaler.fit_transform(feature)
+    return feature
+
+def generategraph(filename,file):
+    plt.figure(figsize=(2.24,2.24))
+    ax = plt.axes()
+    ax.set_axis_off()
+    savefile = filename + '.png'  # file might need to be replaced by a string
+    full_file_name = os.path.join(app.config['UPLOAD_PLT'], savefile)
+    plt.imshow(file)
+    plt.savefig(full_file_name)
+    return savefile 
+
+def printPredict(feature):
+    y_pred = model.predict(feature)
+    for i,label in enumerate(y_pred):
+        if label <= 0.5:
+            predicted = 'Negative'
+        else :
+            predicted = 'Positive'
+    return '<p>Predicted:{0}\n{1} Covid-19</p>'.format(label,predicted)
